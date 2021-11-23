b1b98e9bfeaea3c4324f6925b800894859e5ff72
albe2669 <albert@risenielsen.dk>
2021-11-23 22:19:44 +0100

    Converted to C# 10

--- SELearning/Program.cs
+++ SELearning/Program.cs
@@ -2,25 +2,68 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
 
-namespace SELearning
+var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+
+var initialScopes = builder.Configuration.GetValue<string>("DownstreamApi:Scopes")?.Split(' ');
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+        .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+            .AddMicrosoftGraph(builder.Configuration.GetSection("DownstreamApi"))
+            .AddInMemoryTokenCaches();
+builder.Services.AddControllersWithViews()
+    .AddMicrosoftIdentityUI();
+
+builder.Services.AddAuthorization(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    // By default, all incoming requests will be authorized according to the default policy
+    options.FallbackPolicy = options.DefaultPolicy;
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor()
+    .AddMicrosoftIdentityConsentHandler();
+
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<IWeatherForecastRepository, WeatherForecastRepository>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment()) {
+    app.UseDeveloperExceptionPage();
+} else {
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapBlazorHub();
+    endpoints.MapFallbackToPage("/_Host");
+});
+
+app.Run();
--- SELearning/Services/WeatherForecastService.cs
+++ SELearning/Services/WeatherForecastService.cs
@@ -2,20 +2,19 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SELearning.Services
+namespace SELearning.Services;
+
+public class WeatherForecastService
 {
-    public class WeatherForecastService
+    private readonly IWeatherForecastRepository _repository;
+
+    public WeatherForecastService(IWeatherForecastRepository repository)
     {
-        private readonly IWeatherForecastRepository _repository;
+        _repository = repository;
+    }
 
-        public WeatherForecastService(IWeatherForecastRepository repository)
-        {
-            _repository = repository;
-        }
-
-        public async Task<WeatherForecastDTO[]> Get()
-        {
-            return await _repository.ReadAsync(DateTime.Now);
-        }
+    public async Task<WeatherForecastDTO[]> Get()
+    {
+        return await _repository.ReadAsync(DateTime.Now);
     }
 }
