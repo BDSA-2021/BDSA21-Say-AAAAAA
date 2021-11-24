a199a0e62c4d5b5b27b552aab05c7f937e7dd459
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-11-24 19:17:12 +0100

    Implement PermissionDecider

--- SELearning.Core/Permission/PermissionDecider.cs
+++ SELearning.Core/Permission/PermissionDecider.cs
@@ -2,8 +2,28 @@ namespace SELearning.Core.Permission;
 
 public class PermissionDecider : IPermissionService
 {
-    public Task<bool> IsAllowed(object user, Permission requestedPermission)
+    private readonly IDictionary<Permission, IEnumerable<Func<object, Task<bool>>>> _permissions;
+
+
+    public PermissionDecider(IDictionary<Permission, IEnumerable<Func<object, Task<bool>>>> permissionRules)
     {
-        throw new NotImplementedException();
+        _permissions = permissionRules;
     }
+
+    public async Task<bool> IsAllowed(object user, Permission requestedPermission)
+    {
+        if (!PermissionHasRules(requestedPermission))
+            return true;
+
+        foreach (var rule in _permissions[requestedPermission])
+        {
+            bool isAllowed = await rule(user);
+            if (!isAllowed)
+                return false;
+        }
+
+        return true;
+    }
+
+    private bool PermissionHasRules(Permission p) => _permissions.ContainsKey(p) && _permissions[p].Count() != 0;
 }
