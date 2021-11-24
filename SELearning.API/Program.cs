70207a1959abe849b1d1c0fc9de626becb04da33
albe2669 <albert@risenielsen.dk>
2021-11-24 16:00:36 +0100

    Converted to webassembly

---
+++ SELearning.API/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+#region Configuration
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+if (builder.Environment.IsDevelopment())
+{
+
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "SELearning.API", Version = "v1" });
+    });
+
+    builder.Services.AddDbContext<WeatherContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SELearning")));
+}
+builder.Services.AddScoped<IWeatherContext, WeatherContext>();
+builder.Services.AddScoped<IWeatherForecastRepository, WeatherForecastRepository>();
+#endregion
+
+var app = builder.Build();
+#region Serve
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapControllers();
+    endpoints.MapFallbackToFile("index.html");
+});
+
+app.Run();
+#endregion
