b410401eedf09b44318d230352ce2cfe58ea7a92
Adrian Borup <adrian@borup.com>
2021-11-26 14:11:05 +0100

    Implement PermissionAttribute

--- SELearning.Infrastructure/Authorization/PermissionAttribute.cs
+++ SELearning.Infrastructure/Authorization/PermissionAttribute.cs
@@ -5,8 +5,12 @@ namespace SELearning.Infrastructure.Authorization;
 
 public class PermissionAttribute : AuthorizeAttribute
 {
+    // TODO: @anti, fix this with your upcoming constants thing
+    const string POLICY_PREFIX = "Permission";
+
     public PermissionAttribute(Permission p)
     {
-
+        var permissionString = Enum.GetName(typeof(Permission), p);
+        Policy = $"{POLICY_PREFIX}{permissionString}";
     }
 }
