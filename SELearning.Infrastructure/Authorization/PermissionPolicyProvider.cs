06488b7a6ee10d5fa21a30f9eeef94ad86764045
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-11-26 14:26:09 +0100

    Implement PermissionPolicyProvider

--- SELearning.Infrastructure/Authorization/PermissionPolicyProvider.cs
+++ SELearning.Infrastructure/Authorization/PermissionPolicyProvider.cs
@@ -1,21 +1,47 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using SELearning.Core.Permission;
 
 namespace SELearning.Infrastructure.Authorization;
 
 public class PermissionPolicyProvider : IAuthorizationPolicyProvider
 {
-    public Task<AuthorizationPolicy> GetDefaultPolicyAsync()
+    private const string POLICY_PREFIX = "Permission";
+
+    private DefaultAuthorizationPolicyProvider DefaultProvider { get; }
+    private AuthorizationOptions Options { get; }
+
+    public PermissionPolicyProvider(IOptions<AuthorizationOptions> options)
     {
-        throw new NotImplementedException();
+        Options = options.Value;
+        DefaultProvider = new DefaultAuthorizationPolicyProvider(options);
     }
 
-    public Task<AuthorizationPolicy?> GetFallbackPolicyAsync()
+    public async Task<AuthorizationPolicy> GetDefaultPolicyAsync() => await DefaultProvider.GetDefaultPolicyAsync();
+
+    public Task<AuthorizationPolicy?> GetFallbackPolicyAsync() => Task.FromResult<AuthorizationPolicy?>(null);
+
+    public async Task<AuthorizationPolicy?> GetPolicyAsync(string policyName) 
     {
-        throw new NotImplementedException();
+        if(!TryParsePolicyPermission(policyName, out Permission parsedPermission))
+            return await DefaultProvider.GetPolicyAsync(policyName); // Could not parse permission... fallback to default implementation
+
+        var policy = new AuthorizationPolicyBuilder();
+        policy.AddRequirements(new PermissionRequirement(parsedPermission));
+
+        return policy.Build();
     }
 
-    public Task<AuthorizationPolicy?> GetPolicyAsync(string policyName)
+    private bool TryParsePolicyPermission(string policyName, out Permission parsedPermission)
     {
-        throw new NotImplementedException();
+        // Permission name
+        if (!policyName.StartsWith(POLICY_PREFIX))
+        {
+            parsedPermission = default(Permission);
+            return false;
+        }
+
+        string permissionName = policyName.Substring(POLICY_PREFIX.Length);
+        return Enum.TryParse<Permission>(permissionName, false, out parsedPermission);
     }
 }
