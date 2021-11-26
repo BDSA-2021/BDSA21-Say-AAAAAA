8c61fc371a4413c08c7ba019d5dcf98e10200a98
Adrian Borup <adrian@borup.com>
2021-11-26 13:48:00 +0100

    Simplify duplicated auth handler tests

--- SELearning.Infrastructure.Tests/Authorization/PermissionAuthorizationHandlerTests.cs
+++ SELearning.Infrastructure.Tests/Authorization/PermissionAuthorizationHandlerTests.cs
@@ -6,8 +6,7 @@ namespace SELearning.Infrastructure.Tests;
 
 public class PermissionAuthorizationHandlerTests
 {
-    [Fact]
-    async public void HandleAsync_GivenPermittedUser_YieldsHasSucceeded()
+    AuthorizationHandlerContext HandleAsync_WithIsPermitted(bool isPermitted)
     {
         var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Name, "homer.simpson") }));
         var requirement = new PermissionRequirement(Permission.CreateContent);
@@ -15,28 +14,25 @@ public class PermissionAuthorizationHandlerTests
         var authContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, null);
 
         var permissionService = new Mock<IPermissionService>();
-        permissionService.Setup(m => m.IsAllowed(user, Permission.CreateContent)).ReturnsAsync(true);
+        permissionService.Setup(m => m.IsAllowed(user, Permission.CreateContent)).ReturnsAsync(isPermitted);
 
         var authHandler = new PermissionAuthorizationHandler(permissionService.Object);
-        await authHandler.HandleAsync(authContext);
+        authHandler.HandleAsync(authContext).Wait();
 
+        return authContext;
+    }
+
+    [Fact]
+    public void HandleAsync_GivenPermittedUser_YieldsHasSucceeded()
+    {
+        var authContext = HandleAsync_WithIsPermitted(true);
         Assert.True(authContext.HasSucceeded);
     }
 
     [Fact]
-    async public void HandleAsync_GivenUnpermittedUser_YieldsHasFailed()
+    public void HandleAsync_GivenUnpermittedUser_YieldsHasFailed()
     {
-        var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Name, "homer.simpson") }));
-        var requirement = new PermissionRequirement(Permission.CreateContent);
-
-        var authContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, null);
-
-        var permissionService = new Mock<IPermissionService>();
-        permissionService.Setup(m => m.IsAllowed(user, Permission.CreateContent)).ReturnsAsync(false);
-
-        var authHandler = new PermissionAuthorizationHandler(permissionService.Object);
-        await authHandler.HandleAsync(authContext);
-
+        var authContext = HandleAsync_WithIsPermitted(false);
         Assert.True(authContext.HasFailed);
     }
 }
