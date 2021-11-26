694215b46f287e2a15893ab8b6ce651005d86703
AmalieBoegild <abso@itu.dk>
2021-11-26 12:13:03 +0100

    Create CommentDetailsDTO

--- SELearning.Core/Comment/CommentDTO.cs
+++ SELearning.Core/Comment/CommentDTO.cs
@@ -1,4 +1,5 @@
 namespace SELearning.Core.Comment
 {
-    public record CommentCreateDTO(string ?author, string ?content);
+    public record CommentCreateDTO(string ?Author, string ?Content);
+    public record CommentDetailsDTO(string ?Author, string ?Content, int Id, DateTime Timestamp, int Rating, int ContentId);
 }
