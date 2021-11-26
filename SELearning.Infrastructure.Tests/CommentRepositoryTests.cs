404b45deeb71774871f828c9ca1f0b1d74ea2725
AmalieBoegild <abso@itu.dk>
2021-11-26 12:13:36 +0100

    Make Repository return tuple

--- SELearning.Infrastructure.Tests/CommentRepositoryTests.cs
+++ SELearning.Infrastructure.Tests/CommentRepositoryTests.cs
@@ -2,6 +2,7 @@ using System;
 using Xunit;
 using Microsoft.EntityFrameworkCore;
 using SELearning.Core.Comment;
+using System.Threading.Tasks;
 
 namespace SELearning.Infrastructure.Tests
 {
@@ -30,6 +31,18 @@ namespace SELearning.Infrastructure.Tests
             );
 
             _context.SaveChanges();
+        }
+
+        [Fact]
+        public async Task AddComment_creates_new_comment_with_generated_id()
+        {
+            CommentCreateDTO comment = new CommentCreateDTO("Harleen","Nice content");
+
+            var created = await _repository.AddComment(comment);
+
+            Assert.Equal(5, created.Item2.Id);
+            Assert.Equal("Harleen",created.Item2.Author);
+            Assert.Equal("Nice content",created.Item2.Content);
         }
     }
 }
--- SELearning.Infrastructure/CommentRepository.cs
+++ SELearning.Infrastructure/CommentRepository.cs
@@ -3,16 +3,24 @@ namespace SELearning.Infrastructure
 {
     public class CommentRepository: ICommentRepository
     {
-        private readonly CommentContext dbContext;
+        private readonly CommentContext _context;
 
         public CommentRepository(CommentContext context)
         {
-            dbContext = context;
+            _context = context;
         }
         //returner created
-        public async Task<OperationResult> AddComment(Comment cmt)
+        public async Task<(OperationResult,CommentDetailsDTO)> AddComment(CommentCreateDTO cmt)
         {
-            return OperationResult.BadRequest;
+            // omdan DTO til comment
+
+            // Tilføj Comment instans yil context.dbset
+
+            // kald savechanges
+
+            //comment har fået et id
+
+            return (OperationResult.BadRequest,new CommentDetailsDTO("","",1,DateTime.Now,1,1));
         }
         //hvis id ikke findes returner notfound, ellers updated
         public async Task<OperationResult> UpdateComment(int Id, Comment cmt)
@@ -29,6 +37,7 @@ namespace SELearning.Infrastructure
 
         public async Task<List<Comment>> GetCommentsByContentId(int contentId)
         {
+            //_context.Comments.Where(x => x.Content.Id == contentId);
             return new List<Comment>();
         }
     }
