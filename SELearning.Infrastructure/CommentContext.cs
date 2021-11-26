f7eca27ca12958c303cbbe4529bdbf1a37379249
AmalieBoegild <abso@itu.dk>
2021-11-26 09:45:59 +0100

    Implement CommentContext

---
+++ SELearning.Infrastructure/CommentContext.cs
@@ -0,0 +1,17 @@
+namespace SELearning.Infrastructure{
+
+    public class CommentContext: DbContext
+    {
+        public DbSet<Comment> Users { get; set; }
+
+        public CommentContext(DbContextOptions<CommentContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Rules for entity creation for DB
+            builder.Entity<Comment>()
+                .HasIndex(t => t.Id)
+                .IsUnique();
+        }
+    }
+}
