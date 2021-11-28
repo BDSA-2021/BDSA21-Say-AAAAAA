994d23bb55cb504f8f4cc00daac3202123ce1cce
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-11-28 13:44:20 +0100

    Add comments and use default provider

--- SELearning.Infrastructure/Authorization/PermissionPolicyProvider.cs
+++ SELearning.Infrastructure/Authorization/PermissionPolicyProvider.cs
@@ -4,21 +4,28 @@ using SELearning.Core.Permission;
 
 namespace SELearning.Infrastructure.Authorization;
 
+/// <summary>
+/// Creates a permission policy
+/// </summary>
 public class PermissionPolicyProvider : IAuthorizationPolicyProvider
 {
     private DefaultAuthorizationPolicyProvider DefaultProvider { get; }
     private AuthorizationOptions Options { get; }
 
-    public PermissionPolicyProvider(IOptions<AuthorizationOptions> options)
-    {
-        Options = options.Value;
-        DefaultProvider = new DefaultAuthorizationPolicyProvider(options);
-    }
+    public PermissionPolicyProvider(IOptions<AuthorizationOptions> options) => DefaultProvider = new DefaultAuthorizationPolicyProvider(options);
 
     public async Task<AuthorizationPolicy> GetDefaultPolicyAsync() => await DefaultProvider.GetDefaultPolicyAsync();
 
-    public Task<AuthorizationPolicy?> GetFallbackPolicyAsync() => Task.FromResult<AuthorizationPolicy?>(null);
+    public async Task<AuthorizationPolicy?> GetFallbackPolicyAsync() => await DefaultProvider.GetFallbackPolicyAsync();
 
+    /// <summary>
+    /// Creates a policy based on the policy name
+    /// </summary>
+    /// <param name="policyName">Name of the policy</param>
+    /// <returns>
+    /// Returns a policy based on the policy name. 
+    /// If the policy name is not recognized, then it will default to Microsoft's DefaultAuthorizationPolicyProvider.GetPolicyAsync()
+    /// </returns>
     public async Task<AuthorizationPolicy?> GetPolicyAsync(string policyName) 
     {
         if(!TryParsePolicyPermission(policyName, out Permission parsedPermission))
@@ -30,6 +37,12 @@ public class PermissionPolicyProvider : IAuthorizationPolicyProvider
         return policy.Build();
     }
 
+    /// <summary>
+    /// Parses the policy name to an enum of type Permission.
+    /// </summary>
+    /// <param name="policyName">Name of the policy</param>
+    /// <param name="parsedPermission">The result of the parsed enum. If it is not able to parse, then it returns the default of the type</param>
+    /// <returns>True if it is parsed and false if not</returns>
     private bool TryParsePolicyPermission(string policyName, out Permission parsedPermission)
     {
         // Permission name
