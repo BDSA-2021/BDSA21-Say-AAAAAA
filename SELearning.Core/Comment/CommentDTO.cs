ae1b2dddf5a1d45a9bc06c521f89152fea38c865
AmalieBoegild <abso@itu.dk>
2021-12-01 15:22:48 +0100

    Change id type from string to int

--- SELearning.Core/Comment/CommentDTO.cs
+++ SELearning.Core/Comment/CommentDTO.cs
@@ -1,6 +1,6 @@
 namespace SELearning.Core.Comment
 {
-    public record CommentCreateDTO(string? Author, string? Text, string ContentId);
+    public record CommentCreateDTO(string? Author, string? Text, int ContentId);
     public record CommentUpdateDTO(string? Text);
 
     public record CommentDetailsDTO(string? Author, string? Text, int Id, DateTime Timestamp, int Rating, Content.Content Content);
