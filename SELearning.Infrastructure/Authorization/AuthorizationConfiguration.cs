afd27b5754ff7b81a02d90ddb027db7521e48512
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-12-02 10:57:15 +0100

    Return permissionbuilder in config class

--- SELearning.Infrastructure/Authorization/AuthorizationConfiguration.cs
+++ SELearning.Infrastructure/Authorization/AuthorizationConfiguration.cs
@@ -6,7 +6,7 @@ namespace SELearning.Infrastructure.Authorization;
 
 public static class AuthorizationConfiguration
 {
-    public static IServiceCollection AddPermissionAuthorization(this IServiceCollection services) 
+    public static PermissionBuilder AddPermissionAuthorization(this IServiceCollection services) 
     {
         // Inject asp net Authorization handler and policy implementations
         services.AddSingleton<IAuthorizationHandler, PermissionAuthorizationHandler>();
@@ -15,6 +15,6 @@ public static class AuthorizationConfiguration
         // Add permission service
         services.AddSingleton<IPermissionService, PermissionDecider>();
 
-        return services;
+        return new(services);
     }
 }
