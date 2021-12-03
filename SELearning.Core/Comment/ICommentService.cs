63859f7a8730e3ac7030c11369963a4a1daa21af
aljb <aljb@itu.dk>
2021-12-03 16:02:52 +0100

    Code cleanup

--- SELearning.Core/Comment/ICommentService.cs
+++ SELearning.Core/Comment/ICommentService.cs
@@ -7,10 +7,7 @@ namespace SELearning.Core.Comment
         Task RemoveComment(int id);
         Task UpvoteComment(int id);
         Task DownvoteComment(int id);
-
         Task<List<Comment>> GetCommentsFromContentId(int contentId);
-
         Task<Comment> GetCommentFromCommentId(int commentId);
-
     }
 }
