2c8abf1e76fd9ccfb9078e3fea25560325b71af9
Joachim Borup <73405008+JoachimBorup@users.noreply.github.com>
2021-12-05 19:19:15 +0100

    [DOCS] Create documentation for the Web API Controllers (#30)
    * Add Not Found response type to CreateComment
    
    * Add CommentController documentation
    
    * Add ContentController documentation
    
    * Clean up comments

--- SELearning.API/Controllers/CommentController.cs
+++ SELearning.API/Controllers/CommentController.cs
@@ -20,38 +20,67 @@ public class CommentController : ControllerBase
         _repository = repository;
     }
 
+    /// <summary>
+    /// <c>GetComment</c> returns the comment with the given ID.
+    /// </summary>
+    /// <param name="ID">The ID of the comment.</param>
+    /// <returns>A comment with the given ID if it exists, otherwise response type 404: Not Found.</returns>
     [HttpGet("{ID}")]
-    [ProducesResponseType(typeof(CommentDTO), 200)] // OK
-    [ProducesResponseType(404)] // Not Found
-    public async Task<ActionResult<CommentDTO>> GetComment(int id)
-        => (await _repository.GetAsync(id)).ToActionResult();
+    [ProducesResponseType(typeof(CommentDTO), 200)]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<CommentDTO>> GetComment(int ID)
+        => (await _repository.GetAsync(ID)).ToActionResult();
 
+    /// <summary>
+    /// <c>GetCommentsByContentID</c> returns all comments in the content with the given content ID.
+    /// </summary>
+    /// <param name="contentID">The ID of the content.</param>
+    /// <returns>A collection of comments in the content if it exists, otherwise response type 404: Not Found.</returns>
     [HttpGet("{contentID}")]
-    [ProducesResponseType(typeof(CommentDTO), 200)] // OK
-    [ProducesResponseType(404)] // Not Found
+    [ProducesResponseType(typeof(CommentDTO), 200)]
+    [ProducesResponseType(404)]
     public async Task<IReadOnlyCollection<CommentDTO>> GetCommentsByContentID(int contentID)
         => await _repository.GetAsyncByContentID(contentID);
 
+    /// <summary>
+    /// <c>CreateComment</c> creates a comment in the content with the given content ID.
+    /// </summary>
+    /// <param name="contentID">The ID of the content.</param>
+    /// <param name="comment">The record of the comment.</param>
+    /// <returns>A response type 201: Created if the content exists, otherwise response type 404: Not Found.</returns>
     [AuthorizePermission(Permission.CreateComment)] // TODO: Create the possibility to have an 'or' evaluation of rules in the permission attribute and policy provider.
     [HttpPost]
-    [ProducesResponseType(201)] // Created
+    [ProducesResponseType(201)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> CreateComment(int contentID, CommentDTO comment)
     {
         var (result, created) = await _repository.CreateAsync(contentID, comment);
+        if (result == OperationResult.NotFound) return result.ToActionResult();
         return CreatedAtRoute(nameof(GetComment), new { created.ID }, created);
     }
 
+    /// <summary>
+    /// <c>UpdateComment</c> updates the comment with the given ID.
+    /// </summary>
+    /// <param name="ID">The ID of the comment.</param>
+    /// <param name="comment">The record of the updated comment.</param>
+    /// <returns>A response type 204: No Content if the comment exists, otherwise response type 404: Not Found.</returns>
     [AuthorizePermission(Permission.EditAnyComment)] // TODO: Create the possibility to have an 'or' evaluation of rules in the permission attribute and policy provider. EditOwnComment missing
     [HttpPut("{ID}")]
-    [ProducesResponseType(204)] // No Content
-    [ProducesResponseType(404)] // Not Found
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> UpdateComment(int ID, CommentDTO comment)
         => (await _repository.UpdateAsync(ID, comment)).Item1.ToActionResult();
 
+    /// <summary>
+    /// <c>DeleteComment</c> deletes the comment with the given ID.
+    /// </summary>
+    /// <param name="ID">The ID of the comment.</param>
+    /// <returns>A response type 204: No Content if the comment exists, otherwise response type 404: Not Found.</returns>
     [AuthorizePermission(Permission.DeleteAnyComment)] // TODO: Create the possibility to have an 'or' evaluation of rules in the permission attribute and policy provider.
     [HttpDelete("{ID}")]
-    [ProducesResponseType(204)] // No Content
-    [ProducesResponseType(404)] // Not Found
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> DeleteComment(int ID)
         => (await _repository.DeleteAsync(ID)).ToActionResult();
 }
--- SELearning.API/Controllers/ContentController.cs
+++ SELearning.API/Controllers/ContentController.cs
@@ -21,32 +21,53 @@ public class ContentController : ControllerBase
         _repository = repository;
     }
 
+    /// <summary>
+    /// <c>GetContent</c> returns the content with the given ID.
+    /// </summary>
+    /// <param name="ID">The ID of the content.</param>
+    /// <returns>A content with the given ID if it exists, otherwise response type 404: Not Found.</returns>
     [HttpGet("{ID}")]
-    [ProducesResponseType(typeof(ContentDTO), 200)] // OK
-    [ProducesResponseType(404)] // Not Found
+    [ProducesResponseType(typeof(ContentDTO), 200)]
+    [ProducesResponseType(404)]
     public async Task<ActionResult<ContentDTO>> GetContent(string ID)
         => (await _repository.GetContent(ID)).ToActionResult();
 
+    /// <summary>
+    /// <c>CreateContent</c> creates a content.
+    /// </summary>
+    /// <param name="content">The record of the content.</param>
+    /// <returns>A response type 201: Created.</returns>
     [AuthorizePermission(Permission.CreateContent)] // TODO: Create the possibility to have an 'or' evaluation of rules in the permission attribute and policy provider.
     [HttpPost]
-    [ProducesResponseType(201)] // Created
+    [ProducesResponseType(201)]
     public async Task<IActionResult> CreateContent(ContentDTO content)
     {
         var (result, created) = await _repository.AddContent(content);
         return CreatedAtRoute(nameof(GetContent), new { created.ID }, created);
     }
 
+    /// <summary>
+    /// <c>UpdateContent</c> updates the content with the given ID.
+    /// </summary>
+    /// <param name="ID">The ID of the content.</param>
+    /// <param name="content">The record of the updated content.</param>
+    /// <returns></returns>
     [AuthorizePermission(Permission.EditAnyContent)] // TODO: Create the possibility to have an 'or' evaluation of rules in the permission attribute and policy provider.
     [HttpPut("{ID}")]
-    [ProducesResponseType(204)] // No Content
-    [ProducesResponseType(404)] // Not Found
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> UpdateContent(string ID, ContentDTO content)
         => (await _repository.UpdateContent(ID, content)).ToActionResult();
 
+    /// <summary>
+    /// <c>GetContent</c> deletes the content with the given ID, and its associated comments.
+    /// </summary>
+    /// <param name="ID">The ID of the content.</param>
+    /// <returns>A response type 204: No Content if the content exists, otherwise response type 404: Not Found.</returns>
     [AuthorizePermission(Permission.DeleteAnyContent)] // TODO: Create the possibility to have an 'or' evaluation of rules in the permission attribute and policy provider.
     [HttpDelete("{ID}")]
-    [ProducesResponseType(204)] // No Content
-    [ProducesResponseType(404)] // Not Found
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
     public async Task<IActionResult> DeleteContent(string ID)
         => (await _repository.DeleteContent(ID)).ToActionResult();
 }
