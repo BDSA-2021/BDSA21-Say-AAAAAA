6baf9bae035a0ced4123c566ff245a6f95e446cd
Joachim Borup <73405008+JoachimBorup@users.noreply.github.com>
2021-12-05 22:35:08 +0100

    Created ContentController and Tests

---
+++ SELearning.API.Tests/ContentControllerTest.cs
@@ -0,0 +1,455 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SELearning.API.Controllers;
+using SELearning.Core.Content;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SELearning.API.Tests;
+
+public class ContentControllerTest
+{
+    [Fact]
+    public async Task GetContent_Given_Valid_ID_Returns_ContentDTO()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        var expected = new ContentDto { Id = 1 };
+        service.Setup(m => m.GetContent(1)).ReturnsAsync(expected);
+
+        // Act
+        var actual = (await controller.GetContent(1)).Value;
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task GetContent_Given_Invalid_ID_Returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        service.Setup(m => m.GetContent(-1)).ThrowsAsync(new Exception());
+
+        // Act
+        var response = (await controller.GetContent(-1)).Result;
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task GetAllContent_Returns_Contents_If_Service_Can_Find()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        var expected = Array.Empty<ContentDto>;
+        service.Setup(m => m.GetContent()).ReturnsAsync(expected);
+
+        // Act
+        var actual = ((await controller.GetAllContent()).Result as OkObjectResult)!.Value;
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task GetAllContent_Returns_NotFound_If_Service_Can_Not_Find()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        service.Setup(m => m.GetContent()).ThrowsAsync(new Exception());
+
+        // Act
+        var response = (await controller.GetContentsBySectionID(-1)).Result;
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task GetContentsBySectionID_Given_Valid_ID_Returns_Contents()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        var expected = Array.Empty<ContentDto>;
+        service.Setup(m => m.GetContentInSection(1)).ReturnsAsync(expected);
+
+        // Act
+        var actual = ((await controller.GetContentsBySectionID(1)).Result as OkObjectResult)!.Value;
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task GetContentsBySectionID_Given_Invalid_ID_Returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        var expected = Array.Empty<ContentDto>;
+        service.Setup(m => m.GetContentInSection(-1)).ThrowsAsync(new Exception());
+
+        // Act
+        var response = (await controller.GetContentsBySectionID(-1)).Result;
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task CreateContent_Returns_CreatedAtRoute_If_Service_Can_Create()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        var content = new ContentCreateDto { Title = "Title" };
+
+        // Act
+        var result = (await controller.CreateContent(content) as CreatedAtRouteResult)!;
+
+        // Assert
+        Assert.Equal("GetComment", result.RouteName);
+        Assert.Equal(content, result.Value);
+    }
+
+    [Fact]
+    public async Task CreateContent_Returns_NotFound_If_Service_Can_Not_Create()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        var content = new ContentCreateDto { Title = "Title" };
+        service.Setup(m => m.AddContent(content)).ThrowsAsync(new Exception());
+
+        // Act
+        var response = await controller.CreateContent(content);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task UpdateContent_Given_Valid_ID_Returns_NoContent()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        // Act
+        var response = await controller.UpdateContent(1, new ContentUpdateDto { Title = "Title" });
+
+        // Assert
+        Assert.IsType<NoContentResult>(response);
+    }
+
+    [Fact]
+    public async Task UpdateContent_Given_Invalid_ID_Returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        var content = new ContentUpdateDto { Title = "Title" };
+        service.Setup(m => m.UpdateContentAsync(-1, content)).ThrowsAsync(new Exception());
+
+        // Act
+        var response = await controller.UpdateContent(-1, content);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task DeleteContent_Given_Valid_ID_Returns_NoContent()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        // Act
+        var response = await controller.DeleteContent(1);
+
+        // Assert
+        Assert.IsType<NoContentResult>(response);
+    }
+
+    [Fact]
+    public async Task DeleteContent_Given_Invalid_ID_Returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        service.Setup(m => m.DeleteContent(-1)).ThrowsAsync(new Exception());
+
+        // Act
+        var response = await controller.DeleteContent(-1);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task UpvoteContent_Given_Valid_ID_Returns_NoContent()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        // Act
+        var response = await controller.UpvoteContent(1);
+
+        // Assert
+        Assert.IsType<NoContentResult>(response);
+    }
+
+    [Fact]
+    public async Task UpvoteContent_Given_Invalid_ID_Returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        service.Setup(m => m.IncreaseContentRatingAsync(-1)).ThrowsAsync(new Exception());
+
+        // Act
+        var response = await controller.UpvoteContent(-1);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task DownvoteContent_Given_Valid_ID_Returns_NoContent()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        // Act
+        var response = await controller.DownvoteContent(1);
+
+        // Assert
+        Assert.IsType<NoContentResult>(response);
+    }
+
+    [Fact]
+    public async Task DownvoteContent_Given_Invalid_ID_Returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        service.Setup(m => m.DecreaseContentRating(-1)).ThrowsAsync(new Exception());
+
+        // Act
+        var response = await controller.DownvoteContent(-1);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task GetSection_Given_Valid_ID_Returns_ContentDTO()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        var expected = new SectionDto { Id = 1 };
+        service.Setup(m => m.GetSection(1)).ReturnsAsync(expected);
+
+        // Act
+        var actual = (await controller.GetSection(1)).Value;
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task GetSection_Given_Invalid_ID_Returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        service.Setup(m => m.GetSection(-1)).ThrowsAsync(new Exception());
+
+        // Act
+        var response = (await controller.GetSection(-1)).Result;
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task GetAllSections_Returns_Sections_If_Service_Can_Find()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        var expected = Array.Empty<SectionDto>;
+        service.Setup(m => m.GetSections()).ReturnsAsync(expected);
+
+        // Act
+        var actual = ((await controller.GetAllSections()).Result as OkObjectResult)!.Value;
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task GetAllSections_Returns_NotFound_If_Service_Can_Not_Find()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        service.Setup(m => m.GetSections()).ThrowsAsync(new Exception());
+
+        // Act
+        var response = (await controller.GetAllSections()).Result;
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task CreateSection_Returns_CreatedAtRoute_If_Service_Can_Create()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        var section = new SectionCreateDto { Title = "Title" };
+
+        // Act
+        var result = (await controller.CreateSection(section) as CreatedAtRouteResult)!;
+
+        // Assert
+        Assert.Equal("GetComment", result.RouteName);
+        Assert.Equal(section, result.Value);
+    }
+
+    [Fact]
+    public async Task CreateService_Returns_NotFound_If_Service_Can_Not_Create()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        var section = new SectionCreateDto { Title = "Title" };
+        service.Setup(m => m.AddSection(section)).ThrowsAsync(new Exception());
+
+        // Act
+        var response = await controller.CreateSection(section);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task UpdateSection_Given_Valid_ID_Returns_NoContent()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        // Act
+        var response = await controller.UpdateSection(1, new SectionUpdateDto { Title = "Title" });
+
+        // Assert
+        Assert.IsType<NoContentResult>(response);
+    }
+
+    [Fact]
+    public async Task UpdateSection_Given_Invalid_ID_Returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        var section = new SectionUpdateDto { Title = "Title" };
+        service.Setup(m => m.UpdateSectionAsync(-1, section)).ThrowsAsync(new Exception());
+
+        // Act
+        var response = await controller.UpdateSection(-1, section);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task DeleteSection_Given_Valid_ID_Returns_NoContent()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        // Act
+        var response = await controller.DeleteSection(1);
+
+        // Assert
+        Assert.IsType<NoContentResult>(response);
+    }
+
+    [Fact]
+    public async Task DeleteSection_Given_Invalid_ID_Returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ContentController>>();
+        var service = new Mock<IContentService>();
+        var controller = new ContentController(logger.Object, service.Object);
+
+        service.Setup(m => m.DeleteSection(-1)).ThrowsAsync(new Exception());
+
+        // Act
+        var response = await controller.DeleteSection(-1);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+}
---
+++ SELearning.API/Controllers/ContentController.cs
@@ -0,0 +1,275 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+using SELearning.Core.Content;
+
+namespace SELearning.API.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+public class ContentController : ControllerBase
+{
+    private readonly ILogger<ContentController> _logger;
+    private readonly IContentService _service;
+
+    public ContentController(ILogger<ContentController> logger, IContentService service)
+    {
+        _logger = logger;
+        _service = service;
+    }
+
+    [Authorize]
+    [HttpGet("{ID}")]
+    [ProducesResponseType(typeof(ContentDto), 200)] // OK
+    [ProducesResponseType(404)] // Not Found
+    public async Task<ActionResult<ContentDto>> GetContent(int ID)
+    {
+        try
+        {
+            return Ok(await _service.GetContent(ID));
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+
+    /// <summary>
+    /// <c>GetAllContent</c> returns all contents.
+    /// </summary>
+    /// <returns>all contents if they can be found, otherwise response type 404: Not Found.</returns>
+    [Authorize]
+    [HttpGet]
+    [ProducesResponseType(typeof(IReadOnlyCollection<ContentDto>), 200)]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<IReadOnlyCollection<ContentDto>>> GetAllContent()
+    {
+        try
+        {
+            return Ok(await _service.GetContent());
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+
+    /// <summary>
+    /// <c>GetContentsBySectionID</c> returns all contents in the section with the given section ID.
+    /// </summary>
+    /// <param name="sectionID">The ID of the section.</param>
+    /// <returns>A collection of contents in the section if it exists, otherwise response type 404: Not Found.</returns>
+    [Authorize]
+    [HttpGet("{sectionID}")]
+    [ProducesResponseType(typeof(IReadOnlyCollection<ContentDto>), 200)]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<IReadOnlyCollection<ContentDto>>> GetContentsBySectionID(int sectionID)
+    {
+        try
+        {
+            return Ok(await _service.GetContentInSection(sectionID));
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+
+    [Authorize]
+    [HttpPost]
+    [ProducesResponseType(201)] // Created
+    [ProducesResponseType(404)] // Not Found
+    public async Task<IActionResult> CreateContent(ContentCreateDto content)
+    {
+        try
+        {
+            await _service.AddContent(content);
+            return CreatedAtRoute(nameof(GetContent), content);
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+
+    /// <returns>A response type 204: No Content if the content exists, otherwise response type 404: Not Found.</returns>
+    [Authorize]
+    [HttpPut("{ID}")]
+    [ProducesResponseType(204)] // No Content
+    [ProducesResponseType(404)] // Not Found
+    public async Task<IActionResult> UpdateContent(int ID, ContentUpdateDto content)
+    {
+        try
+        {
+            await _service.UpdateContentAsync(ID, content);
+            return NoContent();
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+
+    [Authorize]
+    [HttpDelete("{ID}")]
+    [ProducesResponseType(204)] // No Content
+    [ProducesResponseType(404)] // Not Found
+    public async Task<IActionResult> DeleteContent(int ID)
+    {
+        try
+        {
+            await _service.DeleteContent(ID);
+            return NoContent();
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+
+    /// <summary>
+    /// <c>UpvoteContent</c> increases the rating of the content with the given ID.
+    /// </summary>
+    /// <param name="ID">The ID of the content.</param>
+    /// <returns>A response type 204: No Content if the content exists, otherwise response type 404: Not Found.</returns>
+    [Authorize]
+    [HttpPut("{ID}")]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> UpvoteContent(int ID)
+    {
+        try
+        {
+            await _service.IncreaseContentRatingAsync(ID);
+            return NoContent();
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+
+    /// <summary>
+    /// <c>DownvoteContent</c> decreases the rating of the content with the given ID.
+    /// </summary>
+    /// <param name="ID">The ID of the content.</param>
+    /// <returns>A response type 204: No Content if the content exists, otherwise response type 404: Not Found.</returns>
+    [Authorize]
+    [HttpPut("{ID}")]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> DownvoteContent(int ID)
+    {
+        try
+        {
+            await _service.DecreaseContentRating(ID);
+            return NoContent();
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+
+    /// <summary>
+    /// <c>GetSection</c> returns the section with the given ID.
+    /// </summary>
+    /// <param name="ID">The ID of the section.</param>
+    /// <returns>A section with the given ID if it exists, otherwise response type 404: Not Found.</returns>
+    [Authorize]
+    [HttpGet("{ID}")]
+    [ProducesResponseType(typeof(SectionDto), 200)]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<SectionDto>> GetSection(int ID)
+    {
+        try
+        {
+            return Ok(await _service.GetSection(ID));
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+
+    /// <summary>
+    /// <c>GetSections</c> returns all sections.
+    /// </summary>
+    /// <returns>all sections if they can be found, otherwise response type 404: Not Found.</returns>
+    [Authorize]
+    [HttpGet]
+    [ProducesResponseType(typeof(IReadOnlyCollection<SectionDto>), 200)]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult<IReadOnlyCollection<SectionDto>>> GetAllSections()
+    {
+        try
+        {
+            return Ok(await _service.GetSections());
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+
+    /// <summary>
+    /// <c>CreateSection</c> creates a section.
+    /// </summary>
+    /// <param name="section">The record of the section.</param>
+    /// <returns>A response type 201: Created.</returns>
+    [Authorize]
+    [HttpPost]
+    [ProducesResponseType(201)]
+    public async Task<IActionResult> CreateSection(SectionCreateDto section)
+    {
+        await _service.AddSection(section);
+        return CreatedAtRoute(nameof(GetSection), section);
+    }
+
+    /// <summary>
+    /// <c>UpdateSection</c> updates the section with the given ID.
+    /// </summary>
+    /// <param name="ID">The ID of the section.</param>
+    /// <param name="section">The record of the updated section.</param>
+    /// <returns>A response type 204: No Content if the section exists, otherwise response type 404: Not Found.</returns>
+    [Authorize]
+    [HttpPut("{ID}")]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> UpdateSection(int ID, SectionUpdateDto section)
+    {
+        try
+        {
+            await _service.UpdateSectionAsync(ID, section);
+            return NoContent();
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+
+    /// <summary>
+    /// <c>DeleteSection</c> deletes the section with the given ID.
+    /// </summary>
+    /// <param name="ID">The ID of the section.</param>
+    /// <returns>A response type 204: No Content if the section exists, otherwise response type 404: Not Found.</returns>
+    [Authorize]
+    [HttpDelete("{ID}")]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> DeleteSection(int ID)
+    {
+        try
+        {
+            await _service.DeleteSection(ID);
+            return NoContent();
+        }
+        catch (Exception)
+        {
+            return NotFound();
+        }
+    }
+}
