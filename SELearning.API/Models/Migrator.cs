13e0ddc2f8e312db2334ade1d4fe055d2082d1c6
albe2669 <albert@risenielsen.dk>
2021-12-08 11:28:33 +0100

    Made Migrator more generic

--- SELearning.API/Models/Migrator.cs
+++ SELearning.API/Models/Migrator.cs
@@ -4,13 +4,18 @@ public static class Migrator
 {
     public static IHost Migrate(this IHost host)
     {
+        Migrator.MigrateContext<WeatherContext>(host);
+        Migrator.MigrateContext<CommentContext>(host);
+
+        return host;
+    }
+
+    private static void MigrateContext<T>(IHost host) where T : DbContext {
         using (var scope = host.Services.CreateScope())
         {
-            using var ctx = scope.ServiceProvider.GetRequiredService<WeatherContext>();
+            using var ctx = scope.ServiceProvider.GetRequiredService<T>();
 
             ctx.Database.Migrate();
         }
-
-        return host;
     }
 }
