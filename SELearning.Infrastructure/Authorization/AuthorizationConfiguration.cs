674f89f8279f44145e7523dbca00938ec3f65dc3
Adrian Borup <adrian@borup.com>
2021-12-08 10:42:39 +0100

    Remove a different part of the code and re-add the old removed

--- SELearning.Infrastructure/Authorization/AuthorizationConfiguration.cs
+++ SELearning.Infrastructure/Authorization/AuthorizationConfiguration.cs
@@ -8,6 +8,10 @@ public static class AuthorizationConfiguration
 {
     public static PermissionBuilder AddPermissionAuthorization(this IServiceCollection services)
     {
+        // Inject asp net Authorization handler and policy implementations
+        services.AddSingleton<IAuthorizationHandler, CredibilityAuthorizationHandler>();
+        services.AddSingleton<IAuthorizationPolicyProvider, PermissionPolicyProvider>();
+
         return new(services);
     }
 }
