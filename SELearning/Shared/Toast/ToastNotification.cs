b7c1af3f2e717287995c6df5faff9fbc9cca00dc
albe2669 <albert@risenielsen.dk>
2021-12-09 14:38:09 +0100

    Implement auto dismissable toast notification

--- SELearning/Shared/Toast/ToastNotification.cs
+++ SELearning/Shared/Toast/ToastNotification.cs
@@ -1,12 +1,17 @@
+using System.Timers;
+
 namespace SELearning.Shared.Toast;
 
 public delegate void OnDismissHandler();
 
-public partial class ToastNotification {
+public partial class ToastNotification : IDisposable {
     public DateTime Created { get; init; } = DateTime.Now;
     public string Title { get; init; }
     public string Body { get; init; }
     public ToastType Type { get; init; }
+
+    private int _duration { get; init; }
+    private Timer _dismissTimer { get; init; }
 
     private IList<OnDismissHandler> _onDismissHandlers = new List<OnDismissHandler>();
 
@@ -16,6 +21,23 @@ public partial class ToastNotification {
         Type = type;
     }
 
+    /// <summary>
+    /// Creates an auto dismissing toast notification. It will dismiss after the given duration.
+    /// </summary>
+    /// <param name="Duration">Amount of miliseconds that should pass before dismissing</param>
+    public ToastNotification(string title, string body, ToastType type, int Duration)
+        : this(title, body, type)
+    {
+        _duration = Duration;
+        _dismissTimer = new Timer();
+        _dismissTimer.Interval = Duration;
+        _dismissTimer.Elapsed += (object sender, ElapsedEventArgs e) => {
+            Dismiss();
+        };
+        _dismissTimer.Start();
+    }
+
+
     public void AddDismissHandler(OnDismissHandler handler) {
         _onDismissHandlers.Add(handler);
     }
@@ -24,5 +46,9 @@ public partial class ToastNotification {
         foreach (var handler in _onDismissHandlers) {
             handler.Invoke();
         }
+    }
+
+    public void Dispose() {
+        _dismissTimer.Dispose();
     }
 }
