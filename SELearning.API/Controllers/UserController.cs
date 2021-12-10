ca3fa49e03597f6150f8f34afeb31e44dc686343
Arnenator <andreotietgen@hotmail.com>
2021-12-10 13:21:53 +0100

    Fix delete of comments and show cred for user (#70)
    * Call OnCommentDelete
    
    * Add credibility to top of the screen
    
    * Add back using
    
    * dotnet format
    
    Co-authored-by: Albert Rise Nielsen <albert@risenielsen.dk>

---
+++ SELearning.API/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+using SELearning.Core.User;
+using SELearning.Infrastructure.Authorization;
+
+namespace SELearning.API.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("/Api/[controller]")]
+[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+public class UserController : ControllerBase
+{
+    private readonly ILogger<UserController> _logger;
+    private readonly IUserRepository _userRepository;
+    private readonly ICredibilityService _credibilityService;
+
+    public UserController(ILogger<UserController> logger, IUserRepository userRepo, ICredibilityService credibilityService)
+    {
+        _logger = logger;
+        _userRepository = userRepo;
+        _credibilityService = credibilityService;
+    }
+
+    [HttpGet("me")]
+    public async Task<ActionResult<User>> GetCurrentUser()
+    {
+        User user = await _userRepository.GetOrAddUser(new UserDTO(User.GetUserId()!, User.FindFirstValue(ClaimTypes.GivenName)));
+
+        return Ok(user);
+    }
+
+    [HttpGet("me/credibility")]
+    public async Task<ActionResult<int>> GetCurrentUserCredibility()
+    {
+        _logger.LogDebug("Getting current user credibility...");
+        int credibility = await _credibilityService.GetCredibilityScore(User);
+        _logger.LogDebug("Current user has a credbility of {credibility}", credibility);
+
+        return Ok(new { CurrentCredibility = credibility });
+    }
+}
