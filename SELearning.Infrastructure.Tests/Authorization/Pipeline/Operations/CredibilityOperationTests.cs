bec9f89b7ee380fe381996a0e5f6447745413f17
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-12-18 20:11:37 +0100

    Add initial CredibilityOperation tests

--- SELearning.Infrastructure.Tests/Authorization/Pipeline/Operations/CredibilityOperationTests.cs
+++ SELearning.Infrastructure.Tests/Authorization/Pipeline/Operations/CredibilityOperationTests.cs
@@ -1,1 +1,47 @@
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using SELearning.Core.Credibility;
+
 namespace SELearning.Infrastructure.Authorization;
+
+public class CredibilityOperationTests
+{
+    private BasePipelineOperation _testPipelineOperation;
+    private ClaimsPrincipal _user;
+    public CredibilityOperationTests()
+    {
+        var permissionService = new Mock<ICredibilityService>();
+        permissionService.Setup(m => m.GetCredibilityScore(_user)).ReturnsAsync(1000);
+
+        var provider = new Mock<IProvider<ICredibilityService>>();
+        provider.Setup(x => x.Get()).Returns(permissionService.Object);
+        
+        _testPipelineOperation = new CredibilityOperation(null, provider.Object);
+        _user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Name, "homer.simpson"), new Claim(ClaimTypes.Role, "Moderator"), new Claim(ClaimTypes.Role, "AnotherOne") }));
+    }
+
+    [Fact]
+    public async Task Invoke_ContextWithNoRequestedPermission_AnEmptyEnumerableWithNoItemsIsAddedToData()
+    {
+        
+        PermissionAuthorizationContext context = new PermissionAuthorizationContext(_user, Enumerable.Empty<Permission>());
+
+        await _testPipelineOperation.Invoke(context);
+        IEnumerable<int> result = context.Data.Get<IEnumerable<int>>("RequiredCredibilityScore");
+
+        Assert.Equal(Enumerable.Empty<int>(), result);
+    }
+
+    [Fact]
+    public async Task Invoke_UserWithModeratorRole_IsModeratorAddedAndSetToFalse()
+    {
+        var userWithModeratorRole = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Name, "homer.simpson"), new Claim(ClaimTypes.Role, "AnotherOne") }));
+        PermissionAuthorizationContext context = new PermissionAuthorizationContext(userWithModeratorRole, Enumerable.Empty<Permission>());
+
+        await _testPipelineOperation.Invoke(context);
+        bool result = context.Data.Get<bool>("IsModerator");
+
+        Assert.False(result);
+    }
+}
